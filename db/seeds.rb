22029468253c9b4c1c29e44ee64ef0a7a7ca6fef
athena.xcy <athena.xcy@gmail.com>
2018-10-05 05:11:05 -0400

    update gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'json'
+require ''
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Course: number code? subject
+ # "subjects": [
+ #      {
+ #        "id": "1161-4900",
+ #        "segment": "2"
+ #      },
+ #      {
+ #        "id": "1161-700",
+ #        "segment": "3"
+ #      },
+ #      {
+ #        "segment": "1",
+ #        "id": "1161-2450"
+ #      }
+ #    ],
+# Subject: name 
+# Instructor: first middle(null) last 
