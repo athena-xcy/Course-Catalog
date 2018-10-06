21d920136bd6ad0c3d285dfb477ce16edeea45d4
athena.xcy <athena.xcy@gmail.com>
2018-10-06 01:57:27 -0400

    add home page and make it root

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-	def get_instructor
-	byebug 
-	i = Instructor.first 
-	instructor = @instructors.first
-	k = @instructors[1]
-	# Instructor.find_each do |instructor|	
-		id = instructor.id			
-		first = instructor.first
-		middle = instructor.middle
-		last = instructor.last 
-		email = instructor.email
-		# 	return id, first, middle, last, email   
-	end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :courses
   resources :subjects
   resources :instructors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'instructors#index'
+  root 'static_pages#home'
 
 	  
 end
